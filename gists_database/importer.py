3c73389d1781ced84f6613b4aa0a6307a531d8a6
spacecowgirl <samantha.walsh@gmail.com>
2019-04-27 13:41:24 +0000

    passing all tests

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,4 +1,27 @@
 import requests
 
+BASE_URL = 'https://api.github.com/users/{username}/gists'
+
 def import_gists_to_database(db, username, commit=True):
-    pass
+    url = BASE_URL.format(username=username)
+    resp = requests.get(url, params={'per_page': 100})
+    if not resp.ok:
+        raise requests.exceptions.HTTPError
+    gists = resp.json()
+    for gist in gists:
+        params = (gist["id"],
+                  gist["html_url"],
+                  gist["git_pull_url"],
+                  gist["git_push_url"],
+                  gist["commits_url"],
+                  gist["forks_url"],
+                  gist["public"],
+                  gist["created_at"],
+                  gist["updated_at"],
+                  gist["comments"],
+                  gist["comments_url"])
+        cursor = db.execute("""insert into gists (github_id, html_url, git_pull_url, 
+        git_push_url, commits_url, forks_url, public, created_at, updated_at, comments, comments_url) VALUES 
+        (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""", params)
+    if commit:
+        db.commit()
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,18 @@
 from .models import Gist
 
 def search_gists(db_connection, **kwargs):
-    pass
+    result = []
+    query = 'SELECT * FROM gists'
+    if kwargs:
+        query += ' WHERE '
+    if 'github_id' in kwargs.keys():
+        query += ' github_id="{}" AND'.format(kwargs['github_id'])
+    if 'created_at' in kwargs.keys():
+        query += ' datetime(created_at) = datetime("{}")'.format(kwargs['created_at'])
+    else:
+        query = query.rstrip(' AND')
+    cursor = db_connection.execute(query)
+    for row in cursor:
+        new_gist = Gist(row)
+        result.append(new_gist)
+    return result
